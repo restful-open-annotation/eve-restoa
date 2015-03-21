c647fdf877cc99baac88d40b88b3cfd9b4b31189
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2015-03-21 11:48:58 +0900

    test document POST and GET

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ port=$(egrep '^PORT *=' settings.py | perl -pe 's/^PORT\s*=\s*(\d+).*/$1/')
 
 ROOT="http://127.0.0.1:$port"
 BASE="$ROOT/annotations"
+DOCS="$ROOT/documents"
 
 # annotation w/Open Annotation context (e.g. "hasBody" instead of "body")
 curl -X POST -i -H 'Content-Type: application/json' -d '
@@ -48,6 +49,21 @@ curl -X GET -i -H 'Accept: application/json' "$BASE/" | ./pretty.py
 
 # get collection as application/ld+json
 curl -X GET -i -H 'Accept: application/ld+json' "$BASE/" | ./pretty.py
+
+# put document
+name=$(python -c '
+from random import choice
+print "".join(choice("abcdefghijklmnopqrstuvwxyz") for _ in range(10))
+')
+curl -X POST -i -H 'Content-Type: application/json' -d '
+{
+  "name": "'"$name"'",
+  "text": "Test document inserted by test.sh\n"
+}' \
+    "$DOCS/" | ./pretty.py
+
+# get the document
+curl -i -H 'Accept: text/plain' "$DOCS/$name"
 
 # get collection as application/rdf+xml
 # curl -X GET -i -H 'Accept: application/rdf+xml' "$BASE/1"
